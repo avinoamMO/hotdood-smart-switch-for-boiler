4377792b83f153274f449dbbc099b7c1b0dff3d5
Avinoam M. Oltchik <36399047+avinoamMO@users.noreply.github.com>
2019-03-23 16:37:49 +0200

    Updates

---
+++ precache-manifest.a5b8d8adf606dda1675c5438b7ce7888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1947c5b06a2159b2b570057cfbf459",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/media/morgan.8a1947c5.png"
+  },
+  {
+    "revision": "45ce2986d79fc7cd05014bd522a88834",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/media/bgimg.45ce2986.jpg"
+  },
+  {
+    "revision": "b1c6f8aa2a293c4ff270",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/js/runtime~main.b1c6f8aa.js"
+  },
+  {
+    "revision": "1f703763517bb74757b4",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/js/main.1f703763.chunk.js"
+  },
+  {
+    "revision": "a9b685ca71a9a3c5ad26",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/js/1.a9b685ca.chunk.js"
+  },
+  {
+    "revision": "1f703763517bb74757b4",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/static/css/main.e8e6f211.chunk.css"
+  },
+  {
+    "revision": "73c4e3187c8c4706ca8754c59fbe4003",
+    "url": "/avinoamMO/hotdood-smart-switch-for-boiler/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/avinoamMO/hotdood-smart-switch-for-boiler/precache-manifest.a5b8d8adf606dda1675c5438b7ce7888.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/avinoamMO/hotdood-smart-switch-for-boiler/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
